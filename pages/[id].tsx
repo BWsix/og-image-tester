c3f5bbc1c796dee20119bb84cf886d9403bf405d
BWsix <itisbw62@gmail.com>
2022-08-19 23:06:46 +0800

    fix: use SSR to get url param

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,18 +1,23 @@
-import type { NextPage } from "next";
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
 const CLHS_NEWS_PAGE = (id: string) =>
   `https://www.clhs.tyc.edu.tw/ischool/public/news_view/show.php?nid=${id}`;
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  id,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  const id = router.query.id;
   const baseUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
 
-  if (typeof id === "string") router.push(CLHS_NEWS_PAGE(id));
+  if (typeof window !== "undefined") router.push(CLHS_NEWS_PAGE(id));
 
   return (
     <div>
@@ -20,11 +25,20 @@ const Home: NextPage = () => {
         <title>壢中官網公告短網址</title>
         <meta name="description" content="壢中官網公告短網址" />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:type" content="website"></meta>
         <meta property="og:image" content={`${baseUrl}/api?id=${id}`} />
       </Head>
       redirecting...
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  return {
+    props: {
+      id: params?.id,
+    },
+  };
 };
 
 export default Home;
